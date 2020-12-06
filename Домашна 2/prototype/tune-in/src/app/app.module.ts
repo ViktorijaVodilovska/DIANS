2a95c5d29200b49e085472ecebfacbcca5c55fd4
Matej Dodevski <matej.ddd@gmail.com>
2020-12-06 15:46:32 +0100

    About us

--- Домашна 2/prototype/tune-in/src/app/app.module.ts
+++ Домашна 2/prototype/tune-in/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { MapBackgroundComponent } from './components/map/map-background/map-background.component';
 import { HeaderMenuComponent } from './components/header/header-menu/header-menu.component';
 import { PlaylistContainerComponent } from './components/playlist/playlist-container/playlist-container.component';
+import { AboutUsComponent } from './components/about/about-us/about-us.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MapBackgroundComponent,
     HeaderMenuComponent,
-    PlaylistContainerComponent
+    PlaylistContainerComponent,
+    AboutUsComponent
   ],
   imports: [
     BrowserModule,
